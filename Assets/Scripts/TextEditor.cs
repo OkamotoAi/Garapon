629151c414ae475d99eb36e05c4503fdd97df4a1
NONONOexe <g.kei0429@gmail.com>
2015-12-17 16:21:32 +0900

    Be well state
    Number will die

--- Assets/Scripts/TextEditor.cs
+++ Assets/Scripts/TextEditor.cs
@@ -1,13 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
 
 public class TextEditor : MonoBehaviour {
 
     public GameObject text;
+    public GameObject newDead;
 
     private int ballNum = 0;
-    private static bool[] isThrough = new bool[72];
+    private const uint Max = 72;
+    private static bool[] isThrough = new bool[Max];
+    private static int[] passed;
+    private static List<int> deadNum;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +24,7 @@ public class TextEditor : MonoBehaviour {
         {
             isThrough[i] = false;
         }
+        deadNum = new List<int>();
 	}
 	
 	// Update is called once per frame
@@ -29,9 +39,15 @@ public class TextEditor : MonoBehaviour {
         Debug.Log(num + "通過");
         if (!isThrough[num - 1])
         {
-            text.GetComponent<Text>().text += num + " ";
-            ballNum++;
             isThrough[num - 1] = true;
+            deadNum.Add(num);
+            deadNum.Sort();
+            ballNum++;
+            string str;
+            str = string.Join(" ", deadNum.Select(x => x.ToString()).ToArray());
+            text.GetComponent<Text>().text = str;
+            newDead.GetComponent<Text>().text = num.ToString();
         }
+
     }
 }
